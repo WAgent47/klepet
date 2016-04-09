69cb9eb0d8bd02d7cfafce875b5e8ce5782f6d18
WAgent47 <aljaz96.kunst@gmail.com>
2016-04-09 19:19:35 +0000

    Zaključek dela na I. nalogi

--- public/js/klepet_UI.js
+++ public/js/klepet_UI.js
@@ -99,6 +99,12 @@ $(document).ready(function() {
     for (var i=0; i < uporabniki.length; i++) {
       $('#seznam-uporabnikov').append(divElementEnostavniTekst(uporabniki[i]));
     }
+    
+    
+    $('#seznam-uporabnikov div').click(function() {
+      $('#poslji-sporocilo').attr('value',('/zasebno ' +'"'+ $(this).text() + '"'));
+      $('#poslji-sporocilo').focus();
+    });
   });
 
   setInterval(function() {
